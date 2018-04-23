d13e26abbc6569df7efdf9613e7d421be29d9ccb
Ronald Vega <ronald.vega48@gmail.com>
2018-04-22 19:13:32 -0600

    Punto Fijo terminado para una funcion

--- puntofijo.m
+++ puntofijo.m
@@ -2,46 +2,35 @@ pkg load symbolic;
 warning('off', 'all');
 #Declarando funciones que se usaran "f(x)=g(x) y g(x) = g'(x)"
 syms f(x) g(x);
-f(x) = sqrt(2*sqrt(x + 1));
+f(x) = 100*(((x/100)^(1.709219858156)) + 0.047571358811426)^ 0.705;
 g(x) = diff(f,x,1);
 
 fx = function_handle(f);
 fxd = function_handle(g);
 #valores del intervalo
-a = 1;
-b = 2;
+a = 0;
+b = 100;
 #valor de epsilon y se agrega un numero limite para las iteraciones
-epsi = 0.001;
-iteraciones = 100;
+epsi = 0.000000000000001;
+iteraciones = 150;
 
-
-#comprobando primer criterio que g(a) y g(b) pertenezcan a [a,b]
-a2 = fx(a); 
-b2 = fx(b);
-
-
-
-#comprobando segundo criterio que g'(a) y g'(b) sean ambos < 1
-    a1 = fxd(a);
-    b1 = fxd(b);
-
-    #Encontrando K
+#Encontrando K
         k = max(abs(a1), abs(b1));
-        fprintf("El valor de K es: %f \n", k)
-        #Asignando el valor de pn para la primera iteracion
+        fprintf("El valor de K es: %f \n", k);
+#Asignando el valor de pn para la primera iteracion
         pn = (a+b)/2;
-        #numero maximo de iteraciones segun formula
-        den = max(b-pn, pn-a);
+#numero maximo de iteraciones segun formula
+        den = max(b-pn, pn-a)
         nmax = log((epsi/den)) / log(k);
-        fprintf("El numero de iteracion esperado es: %f \n", ceil(nmax));
+#       fprintf("El numero de iteracion esperado es: %f \n", ceil(nmax));
 
-        #Comenzando a iterar utilizando p0#
+        #Comenzando a iterar utilizando pn
         printf("%15s | %15s | %15s | %15s |\n","n","Pn","Pn+1", "Error")
         cont = 0;
         while ( cont < iteraciones)
             pn1 = fxd(pn);
             err = abs(pn1 - pn);
-            fprintf("%15f | %15f | %15f | %15f |\n", cont, pn, pn1, err);
+            fprintf("%15f | %15f | %15f | %15e |\n", cont, pn, pn1, err);
             if( err <= epsi )
             pn = pn1;
             fprintf("La solucion encontrada es: %f \n", pn);
@@ -50,6 +39,4 @@ b2 = fx(b);
             pn = pn1;
             cont++;
         endwhile
-
-
 
